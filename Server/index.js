27597780265bc1808fc0025ab8a96ad3fc023283
Marta Molina <marta.molina@live.u-tad.com>
2022-02-07 17:44:30 +0100

    Fix 'usefindandmodifynotsupported'

--- Server/index.js
+++ Server/index.js
@@ -1,45 +1,43 @@
-let express = require('express'),
-  path = require('path'),
-  mongoose = require('mongoose'),
-  cors = require('cors'),
-  bodyParser = require('body-parser'),
-  mongoDb = require('./database/db');
+let express = require("express"),
+  path = require("path"),
+  mongoose = require("mongoose"),
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  mongoDb = require("./database/db");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongoDb.db, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-}).then(() => {
-    console.log('Database sucessfully connected ')
+mongoose.connect(mongoDb.db).then(
+  () => {
+    console.log("Database sucessfully connected ");
   },
-  error => {
-    console.log('Database error: ' + error)
+  (error) => {
+    console.log("Database error: " + error);
   }
-)
+);
 
-const bookRoute = require('./routes/recipe.routes')
+const bookRoute = require("./routes/recipe.routes");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cors());
 
 // Static directory path
-app.use(express.static(path.join(__dirname, 'dist/island_cook')));
-
+app.use(express.static(path.join(__dirname, "dist/island_cook")));
 
 // API root
-app.use('/api', bookRoute)
+app.use("/api", bookRoute);
 
 // PORT
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log('Listening on port ' + port)
-})
+  console.log("Listening on port " + port);
+});
 
 // 404 Handler
 app.use((req, res, next) => {
@@ -47,12 +45,12 @@ app.use((req, res, next) => {
 });
 
 // Base Route
-app.get('/', (req, res) => {
-  res.send('invaild endpoint');
+app.get("/", (req, res) => {
+  res.send("invaild endpoint");
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/island_cook/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/island_cook/index.html"));
 });
 
 // error handler
